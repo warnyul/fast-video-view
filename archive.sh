3e1cf5cb5a16a1c39810a14424ae56ad7378bef9
Balazs Varga <warnyul@gmail.com>
2015-02-26 21:42:56 +0100

    Try fix upload archives.

--- archive.sh
+++ archive.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ -z $"{TRAVIS_TAG}" ]; then
+    echo "upload snapshot"
     ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}"
 else
-    ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="${SIGNING_KEY_RING_FILE}"
+    echo "upload release"
+    # ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="${SIGNING_KEY_RING_FILE}"
 fi
+
