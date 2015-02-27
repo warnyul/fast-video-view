91ee072c2e4da08693deb085d920ff2d6d23bc7c
Balazs Varga <warnyul@gmail.com>
2015-02-27 19:38:32 +0100

    Fix archive sh.

--- archive.sh
+++ archive.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [[ -n $"{TRAVIS_TAG}" ]]; then
-    echo "upload snapshot"
-    ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}"
+    if [[ $"{TRAVIS_BRANCH}" == "develop" ]]; then
+        echo "upload snapshot"
+        ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}"
+    fi
 else
     echo "upload release"
-    # ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="${SIGNING_KEY_RING_FILE}"
+    ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="${SIGNING_KEY_RING_FILE}"
 fi
 
