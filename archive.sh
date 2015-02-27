20562e659deb3220e760d4a673b764b22e79ae6f
Balazs Varga <warnyul@gmail.com>
2015-02-27 20:47:54 +0100

    fix if statement.

--- archive.sh
+++ archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -n $"{TRAVIS_TAG}" ]]; then
-    if [[ $"{TRAVIS_BRANCH}" = "develop" ]]; then
+    if [[ $"{TRAVIS_BRANCH}"="develop" ]]; then
         echo "upload snapshot"
         ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}"
     fi
@@ -8,4 +8,3 @@ else
     echo "upload release"
     ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="${SIGNING_KEY_RING_FILE}"
 fi
-
