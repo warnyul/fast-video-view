c42707dbd400013494c017ffe726620b3e5b1a2e
Balazs Varga <warnyul@gmail.com>
2015-05-23 18:52:35 +0200

    Fix shell scripts.

--- archive.sh
+++ archive.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [[ -n $"{TRAVIS_TAG}" ]]; then
-    if [[ $"{TRAVIS_BRANCH}"="develop" ]]; then
+
+if [ -z "${TRAVIS_TAG}" ]; then
+    if [ "${TRAVIS_BRANCH}" == "develop" ]; then
         echo "upload snapshot"
         ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}"
     fi
 else
     echo "upload release"
-    openssl aes-256-cbc -K $encrypted_1d5880f7ca01_key -iv $encrypted_1d5880f7ca01_iv -in ./video/fast-video-view.gpg.enc -out ./video/fast-video-view.gpg -d
+    ./gradlew :video:uploadArchives -PNEXUS_USERNAME="${NEXUS_USERNAME}" -PNEXUS_PASSWORD="${NEXUS_PASSWORD}" -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile="${SIGNING_KEY_RING_FILE}"
 fi
--- before.sh
+++ before.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod +x ./gradlew
 chmod +x ./archive.sh
-if [[ -z $"{TRAVIS_TAG}" ]]; then
+if [ -z $"{TRAVIS_TAG}" ]; then
     echo "prepare signing"
     openssl aes-256-cbc -K $encrypted_0cd30212d19a_key -iv $encrypted_0cd30212d19a_iv -in ./video/fast-video-view.gpg.enc -out ./video/fast-video-view.gpg -d
 fi
